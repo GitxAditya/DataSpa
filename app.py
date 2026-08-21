54d5d2244fe9a8fdd9e87929622178af1b00b45d
Aditya Kumar <yashwantkumaraditya2007@gmail.com>
2026-08-21 23:29:07 +0530

    Added basic statistics, missing values, Handle missing values (drop / fill)
    Added functionality to handle missing values and display basic statistics for numeric columns.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+# streamlit run app.py
 
 st.set_page_config(page_title="CSV Duplicate Remover", page_icon="💦", layout="wide")
 st.title("DataSpa - CSV Duplicate Remover")
@@ -14,6 +15,53 @@ if uploaded_file is not None:
     st.subheader("Original Data")
     st.write(f"Total rows: **{len(df)}**")
     st.dataframe(df.head(10))
+
+    # basic stats
+    st.subheader("Basic Statistics")
+    st.write("Summary table (mean, median, std, min, max) for numeric columns:")
+    st.dataframe(df.describe())
+
+    # missing values
+    st.subheader("Missing Values")
+    missing_values = df.isnull().sum()
+    missing_values = missing_values[missing_values > 0]
+    if len(missing_values) > 0:
+        st.write("Columns with missing values:")
+        st.dataframe(missing_values)
+    else:
+        st.write("No missing values found.")
+
+    # Handle Missing Values
+    option = st.radio(
+        "Choose how to handle missing values:",
+        ("Do nothing", "Drop missing value rows", "Fill Mean", "Fill Median", "Fill Mode")
+    )
+    cleaned_df = df.copy()
+
+    if option == "Drop missing value rows":
+        cleaned_df = cleaned_df.dropna()
+        st.info("Dropped rows with missing values.")
+
+    elif option == "Fill Mean":
+        numeric_cols = cleaned_df.select_dtypes(include='number').columns
+        for col in numeric_cols:
+            mean_value = cleaned_df[col].mean()
+            cleaned_df[col].fillna(mean_value, inplace=True)
+        st.info("Filled missing values with mean for numeric columns.")
+
+    elif option == "Fill Median":
+        numeric_cols = cleaned_df.select_dtypes(include='number').columns
+        for col in numeric_cols:
+            median_value = cleaned_df[col].median()
+            cleaned_df[col].fillna(median_value, inplace=True)
+        st.info("Filled missing values with median for numeric columns.")
+
+    elif option == "Fill Mode":
+        for col in cleaned_df.columns:
+            mode_value = cleaned_df[col].mode()[0]
+            cleaned_df[col].fillna(mode_value, inplace=True)
+        st.info("Filled missing values with mode for all columns.")
+
     
     # Remove duplicates
     cleaned_df = df.drop_duplicates()
